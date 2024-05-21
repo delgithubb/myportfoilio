7ede0e36716c57f118122178108f476fa056ac74
delgithubb <davoduko25@gmail.com>
2024-05-21 16:23:29 +0100

    Create test.cs

---
+++ test.cs
@@ -0,0 +1,39 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class actviity{
+    
+    public string getGender(){
+        public char gender = Console.ReadLine();
+        if (gender == 'f'){
+            gender = 'm';
+        }
+        if (gender == 'm'){
+            gender = 'f';
+        }
+        return gender;
+    }
+    public string getAge(){
+        public int age = Console.ReadLine();
+        string agestring = (str) age;
+        return agestring;
+    }
+    public string getName(){
+        public string name = Console.ReadLine();
+        return name;
+    }
+ 
+    public static void Main(string[] args)
+    {
+        activity GetInfo = new activity()
+        public string name = GetInfo.getName();
+        public string age = GetInfo.getAge();
+        public string gender = GetInfo.getGender();
+        public string output = name+agestring+gender;
+        public string main void = na
+    
+            
+    }
+}
